99b8c07280e225de2bf5cda1bd0f851cdf4f2c14
VVolkov833 <vadim.volkov.dev@gmail.com>
2024-07-07 22:04:23 +0200

    setup eslint

--- tasks/.eslintrc.cjs
+++ tasks/.eslintrc.cjs
@@ -5,14 +5,31 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react-refresh', 'react', '@typescript-eslint', 'react-compiler'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react/prop-types': 'off', // Optional: You might not need prop-types validation when using TypeScript
+    'react/react-in-jsx-scope': 'off', // Not needed in React 17+ with new JSX transform
+    'react-compiler/react-compiler': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
